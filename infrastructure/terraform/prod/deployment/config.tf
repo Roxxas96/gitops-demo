18d14f09386d0fccd9eb6de9107d6e5b5970fd32
Alexandre Gomez <gomez.a.corneille@gmail.com>
2023-05-13 16:30:16 +0200

    feat: configure deployment
    Configured argocd for application deployment
    
    Signed-off-by: Alexandre Gomez <gomez.a.corneille@gmail.com>

---
+++ infrastructure/terraform/prod/deployment/config.tf
@@ -0,0 +1,26 @@
+data "vault_kv_secret_v2" "gitops-demo_ssh-key" {
+  mount = "kvv2"
+  name  = "prod/deployment/repositories/gitops-demo/ssh-key"
+}
+
+data "vault_kv_secret_v2" "app_database-password" {
+  mount = "kvv2"
+  name  = "prod/app/word-service/database/password"
+}
+
+resource "helm_release" "argocd-config" {
+  name      = "argocd-config"
+  namespace = data.kubernetes_namespace_v1.argocd.metadata[0].name
+
+  chart  = "../../../helm/argo-cd-config/chart"
+  values = ["${file("../../../helm/argo-cd-config/prod.values.yaml")}"]
+
+  set_sensitive {
+    name  = "repositories.gitops-demo.ssh.privateKey"
+    value = data.vault_kv_secret_v2.gitops-demo_ssh-key.data["private_key"]
+  }
+
+  depends_on = [
+    helm_release.argocd,
+  ]
+}
---
+++ infrastructure/terraform/prod/deployment/tools.tf
@@ -0,0 +1,73 @@
+data "kubernetes_namespace_v1" "argocd" {
+  metadata {
+    name = "argocd"
+  }
+}
+
+data "vault_kv_secret_v2" "argocd_admin-password" {
+  name  = "prod/deployment/argocd/admin-password"
+  mount = "kvv2"
+}
+
+resource "helm_release" "argocd" {
+  name      = "argocd"
+  namespace = data.kubernetes_namespace_v1.argocd.metadata[0].name
+
+  chart  = "../../../helm/argo-cd/chart"
+  values = ["${file("../../../helm/argo-cd/prod.values.yaml")}"]
+
+  set_sensitive {
+    name = "configs.secret.argocdServerAdminPassword"
+    # Bcrypt hash of the password
+    value = data.vault_kv_secret_v2.argocd_admin-password.data["password"]
+  }
+}
+
+data "kubernetes_service_v1" "argocd" {
+  metadata {
+    name      = "argocd-server"
+    namespace = data.kubernetes_namespace_v1.argocd.metadata[0].name
+  }
+
+  depends_on = [
+    helm_release.argocd
+  ]
+}
+
+resource "kubernetes_ingress_v1" "argocd" {
+  metadata {
+    name      = "argocd"
+    namespace = data.kubernetes_namespace_v1.argocd.metadata[0].name
+
+    annotations = {
+      "kubernetes.io/ingress.class"                  = "nginx"
+      "cert-manager.io/cluster-issuer"               = "letsencrypt-issuer"
+      "nginx.ingress.kubernetes.io/backend-protocol" = "HTTPS"
+    }
+  }
+
+  spec {
+    tls {
+      hosts       = ["argo.${var.domain}"]
+      secret_name = "argocd-tls"
+    }
+    rule {
+      host = "argo.${var.domain}"
+
+      http {
+        path {
+          path = "/"
+
+          backend {
+            service {
+              name = data.kubernetes_service_v1.argocd.metadata[0].name
+              port {
+                number = data.kubernetes_service_v1.argocd.spec[0].port[0].port
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
